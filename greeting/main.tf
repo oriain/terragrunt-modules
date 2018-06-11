9d43418e49b5e716994fb7d47e3b83efd80017cd
Ryan Robertson <rjr.devry@gmail.com>
2018-06-10 23:09:35 -0700

    Cleanup repo.

--- greeting/main.tf
+++
@@ -1,9 +0,0 @@
-provider "null" {
-  version = "~> 1.0"
-}
-
-resource "null_resource" "greeting" {
-  provisioner local-exec {
-    command = "echo ${var.greeting} ${var.name}"
-  }
-}
--- greeting/vars.tf
+++
@@ -1,7 +0,0 @@
-variable "greeting" {
-  description = "Whether you want to say hello or goodbye."
-}
-
-variable "name" {
-  description = "The name of the person to whom the greeting is addressed."
-}
